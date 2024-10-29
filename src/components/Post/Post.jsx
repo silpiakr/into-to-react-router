570716d993e6a6d36fecb67beb0b1f5a46d2f2a1
silpiAkter <silpiakter461@gmail.com>
2024-10-29 23:52:43 +0600

    added go back function

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,14 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({post}) => {
     const {id, title} = post;
+
+    const navigate = useNavigate();
+
+    const handleReadMore = () => {
+        navigate(`/post/${id}`)
+    }
     return (
         <div className="border-2 rounded py-2 px-6 text-left">
             <h2>Post of Id: {id}</h2>
@@ -10,6 +16,7 @@ const Post = ({post}) => {
             <Link to={`/post/${id}`}>
                 <button className="px-6 py-2 text-white font-bold rounded bg-slate-400 my-5">Show Details</button>
             </Link>
+            <button onClick={handleReadMore}>Read More</button>
         </div>
     );
 };
--- src/components/PostDetailes/PostDetailes.jsx
+++ src/components/PostDetailes/PostDetailes.jsx
@@ -1,14 +1,21 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetailes = () => {
     const post = useLoaderData();
-    const {id, title, body} = post
+    const {id, title, body} = post;
+
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
     return (
         <div className="py-20">
             <div className="border-2 border-red-300 rounded-xl py-2 px-6 text-left space-y-2">
             <h2>Post Detailes about: {id}</h2>
             <p>Title: {title}</p>
             <p>{body}</p>
+            <button className="border-2 border-[#008080] rounded-lg py-1 px-4 " onClick={handleGoBack}>Go Back</button>
             </div>
         </div>
     );
